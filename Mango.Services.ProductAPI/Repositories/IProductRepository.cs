ad9de2a4f1093471369065bf24678fb9f56e0182
Zakaria Masud <zakaria.masud@surbanajurong.com>
2022-12-20 02:06:37 +0600

    Products API Advance Setup: Product Repository Interface

--- Mango.Services.ProductAPI/Repositories/IProductRepository.cs
+++ Mango.Services.ProductAPI/Repositories/IProductRepository.cs
@@ -1,5 +1,11 @@
-﻿namespace Mango.Services.ProductAPI.Repositories;
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+
+namespace Mango.Services.ProductAPI.Repositories;
 
 public interface IProductRepository
 {
+    Task<IEnumerable<ProductDto>> GetProducts();
+    Task<ProductDto> GetProductById(int productId);
+    Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+    Task<bool> DeleteProduct(int productId);    
 }
