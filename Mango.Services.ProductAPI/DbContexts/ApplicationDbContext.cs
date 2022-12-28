ee5114a9eb2e033d1f8172759f7408b0ff9f38f3
Zakaria Masud <zakaria.masud@surbanajurong.com>
2022-12-29 03:52:05 +0600

    Products API Advance Setup: Azure Storage Explorer for Images and migrate product seed data to db

--- Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -21,7 +21,7 @@ public class ApplicationDbContext : DbContext
             Name = "Samosa",
             Price = 15,
             Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "https://zakariabijoy.blob.core.windows.net/mango/14.jpg",
             CategoryName = "Appetizer"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -30,7 +30,7 @@ public class ApplicationDbContext : DbContext
             Name = "Paneer Tikka",
             Price = 13.99,
             Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "https://zakariabijoy.blob.core.windows.net/mango/12.jpg",
             CategoryName = "Appetizer"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -39,7 +39,7 @@ public class ApplicationDbContext : DbContext
             Name = "Sweet Pie",
             Price = 10.99,
             Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "https://zakariabijoy.blob.core.windows.net/mango/11.jpg",
             CategoryName = "Dessert"
         });
         modelBuilder.Entity<Product>().HasData(new Product
@@ -48,7 +48,7 @@ public class ApplicationDbContext : DbContext
             Name = "Pav Bhaji",
             Price = 15,
             Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "https://zakariabijoy.blob.core.windows.net/mango/13.jpg",
             CategoryName = "Entree"
         });
     }
