6bc81811e45a1fa82dad900d6ae8e17ce18bb9ee
Zakaria Masud <zakaria.masud@surbanajurong.com>
2022-12-30 03:40:44 +0600

    Products API Advance Setup: HttpClient Configuration, fontawesome added, design modification in Mango Web Project (FE)

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddHttpClient<IProductService, ProductService>();
 SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
 builder.Services.AddScoped<IProductService, ProductService>();
 
