d5c4670e081307703d672f2802fff3ddb35a6fda
Zakaria Masud <zakaria.masud@surbanajurong.com>
2022-12-30 03:15:26 +0600

    Products API Advance Setup: Product Service in Mango Web Project (FE)

---
+++ Mango.Web/Services/ProductService.cs
@@ -0,0 +1,66 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dtos;
+using Mango.Web.Services.IServices;
+
+namespace Mango.Web.Services;
+
+public class ProductService : BaseServie, IProductService
+{
+    private readonly IHttpClientFactory _httpClient;
+
+    public ProductService(IHttpClientFactory httpClient) : base(httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<T> CreateProductAsync<T>(ProductDto productDto)
+    {
+        return await SendAsync<T>(new APIRequest
+        {
+            ApiType = SD.APIType.POST,
+            Data = productDto,
+            Url = SD.ProductAPIBase + "/api/products",
+            AccessToken = ""
+        });
+    }
+
+    public async Task<T> DeleteProductAsync<T>(int id)
+    {
+        return await SendAsync<T>(new APIRequest
+        {
+            ApiType = SD.APIType.DELETE,
+            Url = SD.ProductAPIBase + "/api/products/" + id,
+            AccessToken = ""
+        });
+    }
+
+    public async Task<T> GetAllProductsAsync<T>()
+    {
+        return await SendAsync<T>(new APIRequest
+        {
+            ApiType = SD.APIType.GET,
+            Url = SD.ProductAPIBase + "/api/products",
+            AccessToken = ""
+        });
+    }
+
+    public async Task<T> GetProductByIdAsync<T>(int id)
+    {
+        return await SendAsync<T>(new APIRequest
+        {
+            ApiType = SD.APIType.GET,
+            Url = SD.ProductAPIBase + "/api/products/" + id,
+            AccessToken = ""
+        });
+    }
+
+    public async Task<T> UpdateProductAsync<T>(ProductDto productDto)
+    {
+        return await SendAsync<T>(new APIRequest
+        {
+            ApiType = SD.APIType.PUT,
+            Data = productDto,
+            Url = SD.ProductAPIBase + "/api/products",
+            AccessToken = ""
+        });
+    }
+}
