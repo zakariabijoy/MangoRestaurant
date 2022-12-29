2f9e6309f1da64f1dc18bb78801304eb57d2a514
Zakaria Masud <zakaria.masud@surbanajurong.com>
2022-12-30 03:24:54 +0600

    Products API Advance Setup: Configure Product Service in Mango Web Project (FE)

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,7 +1,14 @@
+using Mango.Web;
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
