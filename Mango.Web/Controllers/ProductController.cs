69c14777923077ee6f6683f8ce44d2926a713c98
Zakaria Masud <zakaria.masud@surbanajurong.com>
2022-12-31 03:44:39 +0600

    Product CRUD in Web Project (FE): Product Controller Get All Action

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Web.Models.Dtos;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> products = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            if (response != null && response.IsSuccess)
+            {
+                products = JsonSerializer.Deserialize<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(products);
+        }
+    }
+}
