2eb2496e9e870e70d780065c53834e6718785714
zakariabijoy <zakaria.bijoy@live.com>
2023-01-02 04:10:08 +0600

    Product CRUD in WEB APP (FE): Create product

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -24,5 +24,23 @@ namespace Mango.Web.Controllers
 
             return View(products);
         }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDto model)
+        {
+            var response = await _productService.CreateProductAsync<ResponseDto>(model);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(model);
+        }
     }
 }
