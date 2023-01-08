7cdbaf50bd128a2c0bc10f7a5db54ca3e7f29ded
zakariabijoy <zakaria.bijoy@live.com>
2023-01-09 00:20:32 +0600

    IndentityServer: Integrate QuickStart in Identity Server Service

---
+++ Mango.Services.IdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mango.Services.IdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
