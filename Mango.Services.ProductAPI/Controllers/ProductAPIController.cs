f8809e247dfc9f4f336d6e537c7892bd3612ca2c
zakariabijoy <zakaria.bijoy@live.com>
2023-01-29 02:22:49 +0600

    Home and Product Details: Index view home controller in web project (FE) and remove authorize attribute from Get, GetById action method of ProductAPI service

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -18,7 +18,6 @@ public class ProductAPIController : ControllerBase
         _response = new ResponseDto();  
     }
 
-    [Authorize]
     [HttpGet]
     public async Task<ResponseDto> Get()
     {
@@ -37,7 +36,6 @@ public class ProductAPIController : ControllerBase
     }
 
     [HttpGet]
-    [Authorize]
     [Route("{id:int}")]
     public async Task<ResponseDto> Get(int id)
     {
