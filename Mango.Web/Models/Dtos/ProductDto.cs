edb669ac947210308851cf011d6b916c3ff58e7e
zakariabijoy <zakaria.bijoy@live.com>
2023-01-29 02:54:29 +0600

    Home and Product Details: Deatils View in web project (FE)

--- Mango.Web/Models/Dtos/ProductDto.cs
+++ Mango.Web/Models/Dtos/ProductDto.cs
@@ -1,11 +1,20 @@
-﻿namespace Mango.Web.Models.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models.Dtos;
 
 public class ProductDto
 {
+    public ProductDto()
+    {
+        Count= 1;
+    }
+
     public int ProductId { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
     public string? Description { get; set; }
     public string CategoryName { get; set; }
     public string ImageUrl { get; set; }
+    [Range(1, 100)]
+    public int Count { get; set; }
 }
