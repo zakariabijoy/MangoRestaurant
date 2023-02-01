a1713378c3286f703ad0f8cbbb160ef7990f2428
zakariabijoy <zakaria.bijoy@live.com>
2023-02-02 01:59:46 +0600

    Shopping Cart API Service: cart repository interface

---
+++ Mango.Services.ShoppingCartAPI/Repositories/CartRepository.cs
@@ -0,0 +1,26 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dtos;
+
+namespace Mango.Services.ShoppingCartAPI.Repositories;
+
+public class CartRepository : ICartRepository
+{
+    public async Task<bool> ClearCart(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CartDto> GetCartByUserId(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> RemoveFromCart(int cartDetailsId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Repositories/ICartRepository.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dtos;
+
+namespace Mango.Services.ShoppingCartAPI.Repositories;
+
+public interface ICartRepository
+{
+    Task<CartDto> GetCartByUserId(string userId);
+    Task<CartDto> CreateUpdateCart(CartDto cartDto);
+    Task<bool> RemoveFromCart(int cartDetailsId);
+    Task<bool> ClearCart(string userId);
+}
