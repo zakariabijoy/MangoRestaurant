9c01384b1fa9d668f43b8863c0467f81016a34f4
zakariabijoy <zakaria.bijoy@live.com>
2023-02-09 02:29:10 +0600

    Shopping Cart API Service: Add to shopping Cart Repository Method

--- Mango.Services.ShoppingCartAPI/Repositories/CartRepository.cs
+++ Mango.Services.ShoppingCartAPI/Repositories/CartRepository.cs
@@ -1,9 +1,22 @@
-﻿using Mango.Services.ShoppingCartAPI.Models.Dtos;
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.DbContexts;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Repositories;
 
 public class CartRepository : ICartRepository
 {
+    private readonly ApplicationDbContext _db;
+    private readonly IMapper _mapper;
+
+    public CartRepository(ApplicationDbContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+
     public async Task<bool> ClearCart(string userId)
     {
         throw new NotImplementedException();
@@ -11,7 +24,34 @@ public class CartRepository : ICartRepository
 
     public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
     {
-        throw new NotImplementedException();
+        Cart cart  = _mapper.Map<Cart>(cartDto);
+
+        //check if product exists in database, if not create it!
+        var prodInDb = await _db.Products.FirstOrDefaultAsync(u => u.ProductId == cartDto.CartDetails.FirstOrDefault()!.ProductId);
+        if (prodInDb == null)
+        {
+            await _db.Products.AddAsync(cart.CartDetails.FirstOrDefault()!.Product);
+            await _db.SaveChangesAsync();
+        }
+
+       //check if header is  null
+       var cartHeaderFromDb = await _db.CartHeaders.FirstOrDefaultAsync(u => u.UserId == cart.CartHeader.UserId);
+
+        if (cartHeaderFromDb == null)
+        {
+            //create header and details
+            _db.CartHeaders.Add(cart.CartHeader);
+            await _db.SaveChangesAsync();
+            cart.CartDetails.FirstOrDefault()!.CartHeaderId = cart.CartHeader.CartHeaderId;
+            cart.CartDetails.FirstOrDefault()!.Product = null;
+            _db.CartDetails.Add(cart.CartDetails.FirstOrDefault()!);
+            await _db.SaveChangesAsync();
+        }
+
+        //if header is not null
+        //check if details has same product 
+        //if it has then update the count 
+        //else create details
     }
 
     public async Task<CartDto> GetCartByUserId(string userId)
