ff092fde329266b418db5f361f6c8eec85658e90
zakariabijoy <zakaria.bijoy@live.com>
2023-02-12 00:47:23 +0600

    Consume Shopping Cart API Service: Define cart service interface

---
+++ Mango.Web/Services/IServices/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Web.Models.Dtos;
+
+namespace Mango.Web.Services.IServices;
+
+public interface ICartService
+{
+    Task<T> GetCartByUserIdAsync<T>(int userId, string? token = null);
+    Task<T> AddToCartAsync<T>(CartDto cartDto, string? token = null);
+    Task<T> UpdateCartAsync<T>(CartDto cartDto, string? token = null);
+    Task<T> RemoveFromCartAsync<T>(int cartId, string? token = null);
+}
