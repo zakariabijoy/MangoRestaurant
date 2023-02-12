01a44fdf7c40019486beb731e4f31fb3825ae919
zakariabijoy <zakaria.bijoy@live.com>
2023-02-13 01:13:16 +0600

    Coupon API Service: Coupon Repository

---
+++ Mango.Services.CouponAPI/Repositories/CouponRepository.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.DbContexts;
+using Mango.Services.CouponAPI.Models.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Repositories;
+
+public class CouponRepository : ICouponRepository
+{
+    private readonly ApplicationDbContext _db;
+    private readonly IMapper _mapper;
+
+    public CouponRepository(ApplicationDbContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+    public async Task<CouponDto> GetCouponByCode(string couponCode)
+    {
+        var couponFromDb = await _db.Coupons.FirstOrDefaultAsync(u => u.CouponCode == couponCode);
+        return _mapper.Map<CouponDto>(couponFromDb);
+    }
+}
---
+++ Mango.Services.CouponAPI/Repositories/ICouponRepository.cs
@@ -0,0 +1,8 @@
+﻿using Mango.Services.CouponAPI.Models.Dtos;
+
+namespace Mango.Services.CouponAPI.Repositories;
+
+public interface ICouponRepository
+{
+    Task<CouponDto> GetCouponByCode(string couponCode);
+}
