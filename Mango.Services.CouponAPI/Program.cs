82b7ab28755ecaf921f6f7bafa79031c9f0096f9
zakariabijoy <zakaria.bijoy@live.com>
2023-02-21 02:44:37 +0600

    Order API: Migration and Add services to the container

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -9,10 +9,6 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-
-// Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // register auto mapper
