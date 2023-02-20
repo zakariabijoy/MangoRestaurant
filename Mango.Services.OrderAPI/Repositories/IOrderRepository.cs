96b16325cdca1b7801f377aedb29e1a61c623f2a
zakariabijoy <zakaria.bijoy@live.com>
2023-02-21 02:52:29 +0600

    Order API: Order Repository Interface

---
+++ Mango.Services.OrderAPI/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Services.OrderAPI.Models;
+
+namespace Mango.Services.OrderAPI.Repositories;
+
+public interface IOrderRepository
+{
+    Task<bool> AddOrder(OrderHeader orderHeader);
+    Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+}
---
+++ Mango.Services.OrderAPI/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.OrderAPI.Models;
+
+namespace Mango.Services.OrderAPI.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    public Task<bool> AddOrder(OrderHeader orderHeader)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+    {
+        throw new NotImplementedException();
+    }
+}
