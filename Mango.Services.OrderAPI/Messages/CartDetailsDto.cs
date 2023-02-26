7b27ee51d1479a35b643104ad49f044c67fe7050
zakariabijoy <zakaria.bijoy@live.com>
2023-02-27 04:20:37 +0600

    Order API: Models for Azure Messaging

---
+++ Mango.Services.OrderAPI/Messages/CartDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Services.OrderAPI.Messages;
+
+namespace Mango.Services.OrderAPI.Messagess;
+
+public class CartDetailsDto
+{
+    public int CartDetailsId { get; set; }
+    public int CartHeaderId { get; set; }
+    public int ProductId { get; set; }
+    public virtual ProductDto? Product { get; set; }
+    public int Count { get; set; }
+}
---
+++ Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.OrderAPI.Messages;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Mango.Services.OrderAPI.Messaging;
+
+public class AzureServiceBusConsumer
+{
+    private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
+    {
+        var message = args.Message;
+        var body = Encoding.UTF8.GetString(message.Body);
+
+        CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body)!;
+    }
+}
