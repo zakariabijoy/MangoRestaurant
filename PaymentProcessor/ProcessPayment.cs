36019554771ae604fc40103dfa308398aa22355e
zakariabijoy <zakaria.bijoy@live.com>
2023-02-28 02:22:19 +0600

    Payments: Create Payment Processor Class Library

---
+++ PaymentProcessor/ProcessPayment.cs
@@ -0,0 +1,10 @@
+﻿namespace PaymentProcessor;
+
+public class ProcessPayment : IProcessPayment
+{
+    public bool PaymentProcessor()
+    {
+        //implement custom logic and get card details etc
+        return true;
+    }
+}
