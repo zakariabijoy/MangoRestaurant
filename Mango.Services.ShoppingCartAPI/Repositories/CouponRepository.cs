5afa0bcdb10d0272094b76baf58cfba35842235c
zakariabijoy <zakaria.bijoy@live.com>
2023-02-28 01:24:51 +0600

    Sync Microservice Communication: Create Coupon Repository Interface

---
+++ Mango.Services.ShoppingCartAPI/Repositories/CouponRepository.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dtos;
+
+namespace Mango.Services.ShoppingCartAPI.Repositories;
+
+public class CouponRepository : ICouponRepository
+{
+    public Task<CouponDto> GetCoupon(string couponName)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Repositories/ICouponRepository.cs
@@ -0,0 +1,8 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dtos;
+
+namespace Mango.Services.ShoppingCartAPI.Repositories;
+
+public interface ICouponRepository
+{
+    Task<CouponDto> GetCoupon(string couponName);
+}
