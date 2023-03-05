cce9d2c2f85d32f434306ebb923fdb5237c40315
zakariabijoy <zakaria.bijoy@live.com>
2023-03-06 02:58:58 +0600

    Queue: Setup Email Service Project Part 1

---
+++ Mango.Services.Email/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,142 @@
+﻿using Azure.Messaging.ServiceBus;
+using Mango.MessageBus;
+using Mango.Services.OrderAPI.Messages;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Repositories;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Mango.Services.Email.Messaging;
+
+public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+{
+    private readonly string _serviceBusConnectionString;
+    private readonly string _CheckoutMessageTopic;
+    private readonly string _subscriptionCheckout;
+    private readonly string _orderpaymentprocesstopic;
+    private readonly string _orderUpdatePaymentResultTopic;
+
+    private readonly EmailRepository _orderRepository;
+    private readonly IConfiguration _configuration;
+    private readonly IMessageBus _messageBus;
+    private readonly ServiceBusProcessor _checkOutProcessor;
+    private readonly ServiceBusProcessor _orderUpdatePaymentStatusProcessor;
+
+    public AzureServiceBusConsumer(EmailRepository orderRepository, IConfiguration configuration, IMessageBus messageBus)
+    {
+        _orderRepository = orderRepository;
+        _configuration = configuration;
+        _messageBus = messageBus;
+
+        _serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+        _CheckoutMessageTopic = _configuration.GetValue<string>("CheckoutMessageTopic");
+        _subscriptionCheckout = _configuration.GetValue<string>("SubscriptionCheckout");
+        _orderpaymentprocesstopic = _configuration.GetValue<string>("OrderPaymentProcessTopic");
+        _orderUpdatePaymentResultTopic = _configuration.GetValue<string>("OrderUpdatePaymentResultTopic");
+
+        var client = new ServiceBusClient(_serviceBusConnectionString);
+        _checkOutProcessor = client.CreateProcessor(_CheckoutMessageTopic);
+        _orderUpdatePaymentStatusProcessor = client.CreateProcessor(_orderUpdatePaymentResultTopic, _subscriptionCheckout);
+    }
+
+    public async Task Start()
+    {
+        _checkOutProcessor.ProcessMessageAsync += OnCheckOutMessageReceived;
+        _checkOutProcessor.ProcessErrorAsync += ErrorHadler;
+        await _checkOutProcessor.StartProcessingAsync();
+
+        _orderUpdatePaymentStatusProcessor.ProcessMessageAsync += OnOrderPaymentUpdateReceived;
+        _orderUpdatePaymentStatusProcessor.ProcessErrorAsync += ErrorHadler;
+        await _orderUpdatePaymentStatusProcessor.StartProcessingAsync();
+    }
+
+    public async Task Stop()
+    {
+        await _checkOutProcessor.StopProcessingAsync();
+        await _checkOutProcessor.DisposeAsync();
+
+        await _orderUpdatePaymentStatusProcessor.StopProcessingAsync();
+        await _orderUpdatePaymentStatusProcessor.DisposeAsync();
+    }
+
+    private Task ErrorHadler(ProcessErrorEventArgs arg)
+    {
+       Console.WriteLine(arg.Exception.ToString());
+       return Task.CompletedTask;
+    }
+
+    private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
+    {
+        var message = args.Message;
+        var body = Encoding.UTF8.GetString(message.Body);
+
+        CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body)!;
+
+        OrderHeader orderHeader = new()
+        {
+            UserId = checkoutHeaderDto.UserId,
+            FirstName = checkoutHeaderDto.FirstName,
+            LastName = checkoutHeaderDto.LastName,
+            OrderDetails = new List<OrderDetails>(),
+            CardNumber = checkoutHeaderDto.CardNumber,
+            CouponCode = checkoutHeaderDto.CouponCode,
+            CVV = checkoutHeaderDto.CVV,
+            DiscountTotal = checkoutHeaderDto.DiscountTotal,
+            Email = checkoutHeaderDto.Email,
+            ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
+            OrderTime = DateTime.Now,
+            OrderTotal = checkoutHeaderDto.OrderTotal,
+            PaymentStatus = false,
+            Phone = checkoutHeaderDto.Phone,
+            PickupDateTime = checkoutHeaderDto.PickupDateTime,
+        };
+
+        foreach (var detailList in checkoutHeaderDto.CartDetails)
+        {
+            OrderDetails orderDetails = new()
+            {
+                ProductId = detailList.ProductId,
+                ProductName = detailList.Product.Name,
+                Price = detailList.Product.Price,
+                Count = detailList.Count
+            };
+            orderHeader.CartTotalItems += detailList.Count;
+            orderHeader.OrderDetails.Add(orderDetails); 
+        }
+
+        await _orderRepository.AddOrder(orderHeader);
+
+        PaymentRequestMessage paymentRequestMessage = new()
+        {
+            Name = orderHeader.FirstName + " " + orderHeader.LastName,
+            CardNumber = orderHeader.CardNumber,
+            CVV = orderHeader.CVV,
+            ExpiryMonthYear= orderHeader.ExpiryMonthYear,
+            OrderId = orderHeader.OrderHeaderId,
+            OrderTotal = orderHeader.OrderTotal,
+            Email = orderHeader.Email,
+        };
+
+        try
+        {
+            await _messageBus.PublishMessage(paymentRequestMessage, _orderpaymentprocesstopic);
+            await args.CompleteMessageAsync(args.Message);
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.ToString());   
+        }
+    }
+
+    private async Task OnOrderPaymentUpdateReceived(ProcessMessageEventArgs args)
+    {
+        var message = args.Message;
+        var body = Encoding.UTF8.GetString(message.Body);
+
+        UpdatePaymentResultMessage paymentResultMessage = JsonConvert.DeserializeObject<UpdatePaymentResultMessage>(body)!;
+
+        await _orderRepository.UpdateOrderPaymentStatus(paymentResultMessage.OrderId, paymentResultMessage.Status);
+        await args.CompleteMessageAsync(args.Message);
+    }
+}
---
+++ Mango.Services.Email/Repositories/IEmailRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Services.OrderAPI.Models;
+
+namespace Mango.Services.Email.Repositories;
+
+public interface IEmailRepository
+{
+    Task<bool> AddOrder(OrderHeader orderHeader);
+    Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+}
