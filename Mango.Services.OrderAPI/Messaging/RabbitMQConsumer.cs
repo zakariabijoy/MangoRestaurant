35d3213814e978dea170a7dfc3d559c9e710239a
zakariabijoy <zakaria.bijoy@live.com>
2023-03-07 14:05:07 +0600

    RabbitMQ: Setup RabbitMQ consumer with backgroud service part 1

---
+++ Mango.Services.OrderAPI/Messaging/RabbitMQConsumer.cs
@@ -0,0 +1,33 @@
+﻿using Mango.Services.OrderAPI.Repositories;
+using RabbitMQ.Client;
+using System.Threading.Channels;
+
+namespace Mango.Services.OrderAPI.Messaging;
+
+public class RabbitMQConsumer : BackgroundService
+{
+    private readonly OrderRepository _orderRepository;
+    private IConnection _connection;
+    private IModel _channel;
+
+    public RabbitMQConsumer(OrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+
+        var connectionFactory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest",
+        };
+
+        _connection = connectionFactory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(queue: "checkoutqueue", false, false, false, null);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
