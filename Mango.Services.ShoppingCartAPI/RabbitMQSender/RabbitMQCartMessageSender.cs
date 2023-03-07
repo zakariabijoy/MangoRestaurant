f55236d027b21b6aece217d83e68bc4c4b2dcd04
zakariabijoy <zakaria.bijoy@live.com>
2023-03-07 12:34:04 +0600

    RabbitMQ: Add RabbitMQ Nuget Package to ShoppingCartAPI Service Project

---
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -0,0 +1,11 @@
+﻿using Mango.MessageBus;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMQSender;
+
+public class RabbitMQCartMessageSender : IRabbitMQCartMessageSender
+{
+    public void SendMessage(BaseMessage message, string queueName)
+    {
+        throw new NotImplementedException();
+    }
+}
